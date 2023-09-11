ac15c382d03edebf6437ad51ac8bc9da62bf7fa9
muhammadhanief <haniefm19@gmail.com>
2023-09-11 10:24:04 +0700

    commit api

---
+++ pages/api/show-pet.ts
@@ -0,0 +1,11 @@
+import { sql } from "@vercel/postgres";
+import { NextApiResponse, NextApiRequest } from "next";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const pets = await sql`SELECT * FROM Pets;`;
+  //   return response.status(200).json({ pets: pets.rows });
+  return response.status(200).json({ pets });
+}
